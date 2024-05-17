5726bacc503fa62fc4089d57c2d3bcd22448cc71
Shafique M <shafique_m@hotmail.co.uk>
2024-05-17 13:03:05 +0100

    updated main.tf

--- main.tf
+++ main.tf
@@ -100,34 +100,6 @@ resource "aws_security_group" "shafique_tech258_sg" {
     cidr_blocks = var.sg_egress_rule.cidr_blocks
   }
 
-  # ingress {
-  #   from_port   = 22
-  #   to_port     = 22
-  #   protocol    = "tcp"
-  #   cidr_blocks = ["0.0.0.0/0"]
-  # }
-
-  # ingress {
-  #   from_port   = 80
-  #   to_port     = 80
-  #   protocol    = "tcp"
-  #   cidr_blocks = ["0.0.0.0/0"]
-  # }
-
-  # ingress {
-  #   from_port   = 3000
-  #   to_port     = 3000
-  #   protocol    = "tcp"
-  #   cidr_blocks = ["0.0.0.0/0"]
-  # }
-
-  # egress {
-  #   from_port   = 0
-  #   to_port     = 0
-  #   protocol    = "-1"
-  #   cidr_blocks = ["0.0.0.0/0"]
-  # }
-
   tags = {
     Name = var.sg_name
   }
